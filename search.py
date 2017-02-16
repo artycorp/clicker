4777370178fc1ce6a9a6d60dfa8973e58964da7d
Belikov Artem <art_corp@list.ru>
2017-02-16 08:09:37 +0500

    Some fixes

--- search.py
+++ search.py
@@ -1,35 +1,30 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 
-import os
-import time
 import datetime
 import traceback
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-from stem import Signal
-from stem.control import Controller
 
-DATE_FORMAT = "%d/%m/%Y %H:%M:%S"
+DATE_FORMAT = "%Y-%m-%d %H:%M"
 
 class NeedRestartTor(Exception):
     pass
 
 class Search:
     logger = None
-    ui_log = None
     browser = None
     profile = None
     query = None
     CITY = u'Perm'
-    link = None
-    iPage = 1 # page number on search
     cntElems = 0 # количество элементов для поиска на странице
     MAX_SERACHED_ITEMS = 5
     YANDEX_XPATH = u"//a[@href='{0}']"
     needRestart = True
+    file = None
 
 
     def __init__(self, query, logger,max_items):
@@ -37,40 +32,30 @@ class Search:
         self.query = query
         self.browser = None
         self.MAX_SERACHED_ITEMS = max_items
-        self.ui_log = open(os.getenv('COUNTER_SETTINGS_PATH','') + "ui.log", "a")
+        self.file = open("query.txt", "a")
 
     def __enter__(self):
         return self
 
     def destroy(self):
         try:
+            self.file.close()
             if not (self.browser is None):
                 self.browser.stop_client()
                 self.browser.quit()
         except Exception as err:
             traceback.print_exc()
-        if not self.ui_log is None:
-            self.ui_log.close()
-            self.ui_log = None
         self.browser = None
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.logger.info("__exit__")
         self.destroy()
 
-    def writeUiLog(self, searcher, qry):
-        d = datetime.datetime.now()
-        self.ui_log.write("{0} | ".format(d.strftime(DATE_FORMAT)))
-        self.ui_log.write("{0} | ".format(searcher.encode("utf8")))
-        self.ui_log.write("{0} | ".format(qry.encode("utf8")))
-
     def clean(self):
         """Reset values to default"""
-        self.iPage = 1
         self.cntElems = 0
 
     def restart(self):
-        self.ui_log = open(os.getenv('COUNTER_SETTINGS_PATH', '') + "ui.log", "a")
         return self.needRestart
 
     def restartTor(self):
@@ -105,20 +90,6 @@ class Search:
         if self.check_errors():
             raise NeedRestartTor("initYandex")
 
-    def submitRegion(self):
-        """push submit region button"""
-        btn = self.browser.find_element_by_xpath("//button[@type='submit']")
-
-        try:
-            ActionChains(self.browser) \
-                .move_to_element(btn) \
-                .send_keys_to_element(btn, Keys.ENTER) \
-                .perform()
-        except:
-            self.logger.error("Submit Region error")
-            return True # need restart set region
-        return False
-
     # search query string in yandex
     def findQueryYA(self):
         input = self.browser.find_element_by_xpath("// input[ @ id = 'text']")
@@ -147,7 +118,8 @@ class Search:
                 break
             # пропустить первый элемент говнокод:(
             if i > 0:
-                print(elem.get_attribute("href"))
+                time = datetime.datetime.now().strftime(DATE_FORMAT)
+                self.file.write('{}-{}-{}\n'.format(time, str(i), elem.get_attribute("href")))
             i = i + 1
 
 
@@ -159,6 +131,7 @@ class Search:
             raise NeedRestartTor("Search")
 
         try:
+            self.file.write(self.query.encode("UTF-8") + "\n")
             self.getCntElems("//li[@data-cid]/div/div[1]/div[1]/a[1][@href]")
         except:
             self.logger.error("error in getCntElems")
@@ -167,4 +140,3 @@ class Search:
             raise NeedRestartTor("Search")
 
         self.needRestart = False
-        self.writeUiLog(searcher="Not found Yandex", qry=self.query)
